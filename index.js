20d5ee58772a7a4ae827aae326dd65ca98a6bf2f
p-reetu <manchekar.preeti@gmail.com>
2021-11-18 14:05:58 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+let x = document.getElementById("recruit");
+
+setTimeout(function(){ x.innerHTML = "Shortlist"; }, 000);
+
+setTimeout(function(){ x.innerHTML = "Interview"; }, 3000);
+
+setTimeout(function(){ x.innerHTML = "Recruit"; }, 6000);
